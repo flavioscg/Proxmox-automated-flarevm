9d3c0eb2a67707c3ec955129dc46c969a7be454e
Flavio Scognamiglio <67764234+flavioscg@users.noreply.github.com>
2024-11-11 16:35:14 +0100

    Update pre-flarevm.ps1

--- pre-flarevm.ps1
+++ pre-flarevm.ps1
@@ -1,5 +1,5 @@
 (New-Object net.webclient).DownloadFile('https://raw.githubusercontent.com/mandiant/flare-vm/main/install.ps1',"$([Environment]::GetFolderPath("Desktop"))\install.ps1")
-Unblock-File .\flarevm.ps1
+Unblock-File .\pre-flarevm.ps1
 
 Set-ExecutionPolicy Unrestricted -Force
 
